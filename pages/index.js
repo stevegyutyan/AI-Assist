55379503aed4766a6570132306fe66a9af7beeac
Steve Gyutyan <48524133+stevegyutyan@users.noreply.github.com>
2022-12-22 05:17:02 -0800

    latest build

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,36 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from 'react';
+
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+  const [apiOutput, setApiOutput] = useState('')
+  const [isGenerating, setIsGenerating] = useState(false)
+
+  const callGenerateEndpoint = async () => {
+   setIsGenerating(true);
+  
+  console.log("Calling OpenAI...")
+  const response = await fetch('/api/generate', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ userInput }),
+  });
+
+  const data = await response.json();
+  const { output } = data;
+  console.log("OpenAI replied...", output.text)
+
+  setApiOutput(`${output.text}`);
+  setIsGenerating(false);
+}
+  const onUserChangedText = (event) => {
+    setUserInput(event.target.value);
+  };
   return (
     <div className="root">
       <Head>
@@ -11,12 +39,36 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Hello I am The Worlds Smartest AI</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>I can answer Tweets, generate text, and complete other tasks.</h2>
           </div>
         </div>
+        <div className="prompt-container">
+            <textarea placeholder="Hi I am your personal AI type in what you want me to write" className="prompt-box" value={userInput} onChange={onUserChangedText} />
+          </div>
+          <div className="prompt-buttons">
+  <a
+    className={isGenerating ? 'generate-button loading' : 'generate-button'}
+    onClick={callGenerateEndpoint}
+  >
+    <div className="generate">
+    {isGenerating ? <span className="loader"></span> : <p>Generate</p>}
+    </div>
+  </a>
+</div>
+  <div className="output">
+    <div className="output-header-container">
+      <div className="output-header">
+        <h3>Results</h3>
+      </div>
+    </div>
+    <div className="output-content">
+      <p>{apiOutput}</p>
+    </div>
+  </div>
+  
       </div>
       <div className="badge-container grow">
         <a
